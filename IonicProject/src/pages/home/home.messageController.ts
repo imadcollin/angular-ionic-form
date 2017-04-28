28fca6a2ef1cc0dcf19f97885944790397448137
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-04-28 17:05:51 +0200

    Add meesageController files

---
+++ IonicProject/src/pages/home/home.messageController.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FormValidation } from './home.validation';
+
+@Component({
+  selector: 'control-messages',
+  template: `<div *ngIf="errorMessage !== null">{{errorMessage}}</div>`
+})
+export class ControlMessagesComponent {
+  @Input() control: FormControl;
+  constructor() { }
+
+  get errorMessage() {
+    for (let propertyName in this.control.errors) {
+      if (this.control.errors.hasOwnProperty(propertyName) && this.control.touched) {
+        return FormValidation.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
+      }
+    }
+    
+    return null;
+  }
+}
+
+
+
