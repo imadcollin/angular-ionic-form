23521d5bb99484a617cc8894a1783e0e8372a9ef
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-04-28 15:43:37 +0200

    Add required properties logic

--- IonicProject/src/pages/home/home.ts
+++ IonicProject/src/pages/home/home.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+//Import these Namespaces/Instances from angular forms 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ //Declare a property called userForm of type any  
+ public  myForm: any;
 
-  constructor(public navCtrl: NavController) {
+/*Pass the instance to the constractor */ 
+  constructor(public navCtrl: NavController,private formBuilder: FormBuilder) {
+ this.myForm = this.formBuilder.group({
+      'email': ['', [Validators.required]],
+      'password': ['', Validators.required]
+   });
 
   }
 
