54569576f19efbd531fed34b402a0fe399db13f2
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-04-29 02:02:41 +0200

    Add first component test

--- IonicProject/test/home.spec.ts
+++ IonicProject/test/home.spec.ts
@@ -2,14 +2,36 @@ describe('1st tests', () => {
   it('true is true', () => expect(true).toBe(true));
 });
 
-describe('1st tests', () => {
-  it('true is true', () => expect(true).toBe(true));
-});
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
 
-describe('1st tests', () => {
-  it('true is true', () => expect(true).toBe(true));
-});
+import { HomePage } from '../src/pages/home/home';
 
-describe('1st tests', () => {
-  it('true is true', () => expect(true).toBe(true));
+describe('BannerComponent (inline template)', () => {
+
+  let _homePage:    HomePage;
+  let fixture: ComponentFixture<HomePage>;
+  let de:      DebugElement;
+  let el:      HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomePage ], // declare the test component
+    });
+
+    fixture = TestBed.createComponent(HomePage);
+
+    _homePage = fixture.componentInstance; //  test instance
+
+    // query for the title <h1> by CSS element selector
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+  });
+
+  it('Email funtion test', () => {
+    //_formValidation. 
+    expect(_homePage.myForm).toBeNull;
+  });
+ 
 });
