47144650bdfabf7bd31a08b4b77f18589b1e3c61
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-05-02 18:30:35 +0200

    Use mailchecker for mail typo

--- IonicProject/src/pages/home/home.validation.ts
+++ IonicProject/src/pages/home/home.validation.ts
@@ -1,14 +1,33 @@
+//import mail check from mailcheck library 
+let mailcheck = require('mailcheck');
+
 export class FormValidation {
-  static emailValidator(control) {
-        // RFC 2822 compliant regex
-        if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
-            return null;
-        } else {
+
+    static emailValidator(control) {
+      
+        var domains = ['gmail.com', 'aol.com'];
+        var secondLevelDomains = ['hotmail']
+        var topLevelDomains = ["com", "net", "org"];
+
+        if (!control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)){
+            
             return { 'invalidEmailAddress': true };
         }
+          if(mailcheck.run({
+                email: control.value,
+                domains: domains,                       // optional
+                topLevelDomains: topLevelDomains,       // optional
+                secondLevelDomains: secondLevelDomains, // optional
+
+            })) 
+            return{  'typeoError': true}
+
+            else
+                return null; 
+            
     }
 
-     static passwordValidator(control) {
+    static passwordValidator(control) {
         // {6,100}           - Assert password is between 6 and 100 characters
         // (?=.*[0-9])       - Assert a string has at least one number
         if (control.value.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
@@ -21,6 +40,8 @@ export class FormValidation {
         let config = {
             'required': 'Required',
             'invalidEmailAddress': 'Invalid email address',
+            'typeoError': 'Invaalid typeo please provide correct typo',
+
             'invalidPassword': 'Invalid password. Password must be at least 6 characters long, and contain a number.',
             'minlength': `Minimum length ${validatorValue.requiredLength}`
         };
