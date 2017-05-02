d704dbfc343ddec4f1c5dc637ecd17bebe50118c
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-05-02 19:52:10 +0200

    Refactor test file

--- IonicProject/test/home.spec.js
+++ IonicProject/test/home.spec.js
@@ -1,7 +1,3 @@
-/**================================================================ *
- * This is an initial test for Home page logic 
-=================================================================== */
-
 
 /**================================================================ *
    * Email Validation testing  with regular expression 
@@ -47,75 +43,29 @@ it('it should return false Missing @ sign and domain', function () {
   --------------PASSWORD VALIDATION TESTING----------------------
 =================================================================== */
 it('it should return true Valid Password  ', function () {
-   function passwordValidator(control) {
-        if (control.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
-            return null;
-        } else {
-            return { 'invalidPassword': true };
-        }
+  function passwordValidator(control) {
+    if (control.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
+      return null;
+    } else {
+      return { 'invalidPassword': true };
     }
-  expect(passwordValidator("abcdABCD1")).toBe(null); 
-  expect(passwordValidator("123abcd")).toBe(null); 
-  expect(passwordValidator("ABCD1234abcd")).toBe(null); 
+  }
+  expect(passwordValidator("abcdABCD1")).toBe(null);
+  expect(passwordValidator("123abcd")).toBe(null);
+  expect(passwordValidator("ABCD1234abcd")).toBe(null);
 });
 /**================================================================ *
   ---Not Valid Cases 
 =================================================================== */
 it('it should return false Valid Password  ', function () {
-   function passwordValidator(control) {
-        if (control.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
-            return null;
-        } else {
-            return { 'invalidPassword': true };
-        }
+  function passwordValidator(control) {
+    if (control.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
+      return null;
+    } else {
+      return { 'invalidPassword': true };
     }
-  expect(passwordValidator("abcd").invalidPassword).toBe(true); 
-  expect(passwordValidator("12345").invalidPassword).toBe(true); 
-  expect(passwordValidator("ABCD").invalidPassword).toBe(true); 
+  }
+  expect(passwordValidator("abcd").invalidPassword).toBe(true);
+  expect(passwordValidator("12345").invalidPassword).toBe(true);
+  expect(passwordValidator("ABCD").invalidPassword).toBe(true);
 });
-
-
-
-/**
- * Component testing not working properly 
- * The problem was detected :
- * "uncaught referenceerror: exports is not defined"
- * This error is related to typescript or ionic version 
- * This bug was for many other people 
- * for checking the document and reading about this please check this link 
- * https://github.com/monounity/karma-typescript/issues/111
- */
-
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By }              from '@angular/platform-browser';
-// import { DebugElement }    from '@angular/core';
-
-// import { HomePage } from '../src/pages/home/home';
-
-// describe('BannerComponent (inline template)', () => {
-
-//   let _homePage:    HomePage;
-//   let fixture: ComponentFixture<HomePage>;
-//   let de:      DebugElement;
-//   let el:      HTMLElement;
-
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ HomePage ], // declare the test component
-//     });
-
-//     fixture = TestBed.createComponent(HomePage);
-
-//     _homePage = fixture.componentInstance; //  test instance
-
-//     // query for the title <h1> by CSS element selector
-//     de = fixture.debugElement.query(By.css('h1'));
-//     el = de.nativeElement;
-//   });
-
-//   it('Email funtion test', () => {
-//     //_formValidation. 
-//     expect(_homePage.myForm).toBeNull;
-//   });
-
-// });
